39e00d8bcd4847b0e6049355f1a26bbcd8cce74d
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2025-04-15 17:44:06 +0900

    DirectX12のオブジェクトを解放した

--- main.cpp
+++ main.cpp
@@ -7,8 +7,10 @@
 #include <d3d12.h>   // 00_05
 #include <dxgi1_6.h> // 00_05
 #include <cassert>   // 00_05
-#pragma comment(lib,"d3d12.lib") // 00_05
-#pragma comment(lib,"dxgi.lib")  // 00_05
+#pragma comment(lib,"d3d12.lib")  // 00_05
+#pragma comment(lib,"dxgi.lib")   // 00_05
+#include <dxgidebug.h>            // 01_03
+#pragma comment(lib,"dxguid.lib") // 01_03
 
 #pragma region 配布
 
@@ -472,6 +474,40 @@ int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
 
 		}
 	}
+
+#pragma region 解放処理(リソースチェックの前) 01_03
+
+	CloseHandle(fenceEvent);
+	fence->Release();
+	rtvDescriptorHeap->Release();
+	swapChainResources[0]->Release();
+	swapChainResources[1]->Release();
+	swapChain->Release();
+	commandList->Release();
+	commandAllocator->Release();
+	commandQueue->Release();
+	device->Release();
+	useAdapter->Release();
+	dxgiFactory->Release();
+#ifdef _DEBUG
+	debugController->Release();
+#endif
+	CloseWindow(hwnd);
+
+#pragma endregion
+
+#pragma region リソースリークチェック(最後の最後に残っているものがないか) *main関数のreturnの直前に行う 01_03
+
+	// リソースリークチェック
+	IDXGIDebug1* debug;
+	if (SUCCEEDED(DXGIGetDebugInterface1(1, IID_PPV_ARGS(&debug)))) {
+		debug->ReportLiveObjects(DXGI_DEBUG_ALL, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_APP, DXGI_DEBUG_RLO_ALL);
+		debug->ReportLiveObjects(DXGI_DEBUG_D3D12, DXGI_DEBUG_RLO_ALL);
+		debug->Release();
+	}
+
+#pragma endregion
 
 	return 0;
 }
