3e83751c8dcc04a58addc47b8fba7b47ca8bd3b1
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2025-04-15 15:30:17 +0900

    ウィンドウを作れた

--- main.cpp
+++ main.cpp
@@ -1,9 +1,124 @@
-#include <Windows.h>
+#include <Windows.h> // 00_01
+#include <string>    // 00_01
+#include <cstdint>   // 00_03
+
+#pragma region 配布
+
+std::wstring ConvertString(const std::string& str) {
+	if (str.empty()) {
+		return std::wstring();
+	}
+
+	auto sizeNeeded = MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), NULL, 0);
+	if (sizeNeeded == 0) {
+		return std::wstring();
+	}
+	std::wstring result(sizeNeeded, 0);
+	MultiByteToWideChar(CP_UTF8, 0, reinterpret_cast<const char*>(&str[0]), static_cast<int>(str.size()), &result[0], sizeNeeded);
+	return result;
+}
+
+std::string ConvertString(const std::wstring& str) {
+	if (str.empty()) {
+		return std::string();
+	}
+
+	auto sizeNeeded = WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), NULL, 0, NULL, NULL);
+	if (sizeNeeded == 0) {
+		return std::string();
+	}
+	std::string result(sizeNeeded, 0);
+	WideCharToMultiByte(CP_UTF8, 0, str.data(), static_cast<int>(str.size()), result.data(), sizeNeeded, NULL, NULL);
+	return result;
+}
+
+#pragma endregion
+
+#pragma region ウィンドウプロシャージャ 00_03
+
+// ウィンドウプロシャージャ
+LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wparam, LPARAM lparam) {
+	// メッセージに応じて固有の処理を行う
+	switch (msg) {
+		// ウィンドウが破壊された
+	case WM_DESTROY:
+		// OSに対してアプリの終了を伝える
+		PostQuitMessage(0);
+		return 0;
+	}
+	// 標準のメッセージ処理を行う
+	return DefWindowProc(hwnd, msg, wparam, lparam);
+}
+
+#pragma endregion
 
 // Windowsアプリでのエントリーポイント(main関数)
 int WINAPI WinMain(HINSTANCE, HINSTANCE, LPSTR, int) {
-    // 出力ウィンドウへの文字出力
-    OutputDebugStringA("Hello, DirectX!\n");
 
-    return 0;
+#pragma region ウィンドウクラスを登録 00_03
+
+	// ウィンドウクラス
+	WNDCLASS wc{};
+	// ウィンドウプロシャージャ
+	wc.lpfnWndProc = WindowProc;
+	// ウィンドウクラス名
+	wc.lpszClassName = L"WindowClass";
+	// インスタンスハンドル
+	wc.hInstance = GetModuleHandle(nullptr);
+	// カーソル
+	wc.hCursor = LoadCursor(nullptr, IDC_ARROW);
+	// ウィンドウクラスを登録する
+	RegisterClass(&wc);
+
+#pragma endregion
+
+#pragma region ウィンドウサイズを決める 00_03
+
+	// クライアントの領域サイズ
+	const int32_t kClientWidth = 1280;
+	const int32_t kClientHeight = 720;
+
+	// ウィンドウサイズを表す構造体にクライアント領域を入れる
+	RECT wrc = { 0,0,kClientWidth,kClientHeight };
+
+	// クライアント領域を元に実際のサイズにwrcしてもらう
+	AdjustWindowRect(&wrc, WS_OVERLAPPEDWINDOW, false);
+
+#pragma endregion
+
+#pragma region ウィンドウを生成して表示 00_03
+
+	// ウィンドウの生成
+	HWND hwnd = CreateWindow(
+		wc.lpszClassName,     // 利用するクラス名
+		L"CG2",               // タイトルバーの文字
+		WS_OVERLAPPEDWINDOW,  // よく見るウィンドウスタイル
+		CW_USEDEFAULT,        // 表示X座標(Windowsに任せる)
+		CW_USEDEFAULT,        // 表示Y座標(WindowsOSに任せる)
+		wrc.right - wrc.left, // ウィンドウ縦幅
+		wrc.bottom - wrc.top, // ウィンドウ横幅
+		nullptr,              // 親ウィンドウハンドル
+		nullptr,              // メニューハンドル
+		wc.hInstance,         // インスタンスハンドル
+		nullptr);             // オプション
+
+	// ウィンドウを表示する
+	ShowWindow(hwnd, SW_SHOW);
+
+#pragma endregion
+
+	// メインループ 00_03
+	MSG msg{};
+	// ウィンドウのxボタンが押されるまでループ
+	while (msg.message != WM_QUIT) {
+		// Windowにメッセージが来てたら最優先で処理させる
+		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE)) {
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		} else { // ゲームの処理
+
+		}
+	}
+
+	return 0;
 }
