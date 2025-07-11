bc323ad0931cf6d3f08c756f70a4e74678295101
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2025-07-11 10:22:49 +0900

    計算シェーダーに処理を書いた

--- resources/shaders/Particles/ComputeShader.hlsl
+++ resources/shaders/Particles/ComputeShader.hlsl
@@ -1,8 +1,12 @@
 #include "Particles.hlsli"
 
+static const uint kMaxParticles = 512;
 RWStructuredBuffer<Particle> gParticles : register(u0);
 
-[numthreads(256, 1, 1)]
-void main( uint3 DTid : SV_DispatchThreadID )
-{
+[numthreads(512, 1, 1)]
+void main( uint3 DTid : SV_DispatchThreadID ) {
+    uint particlesIndex = DTid.x;
+    if (particlesIndex < kMaxParticles) {
+        gParticles[particlesIndex] = (Particle) 0;
+    }
 }
