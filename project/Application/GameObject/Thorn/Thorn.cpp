f5e2f995034c71b3a616f2aeeafc2c7f953cc71d
Ratta38 <k024g1030@g.neec.ac.jp>
2025-10-18 19:07:07 +0900

    画像張り替え他

--- project/Application/GameObject/Thorn/Thorn.cpp
+++ project/Application/GameObject/Thorn/Thorn.cpp
@@ -26,10 +26,34 @@ void Thorn::Update() {
 		return;
 
 	// トゲのサイズを更新
-	UpgradeThorn();
+	//UpgradeThorn();
+
+	if (scaleTween_.active) {
+		scaleTween_.elapsed += 1.7f * deltaTime_;
+		float t = std::min(scaleTween_.elapsed / scaleTween_.duration, 1.0f);
+
+		transform_.scale.x = Easing::Lerp(scaleTween_.startScale.x, scaleTween_.targetScale.x, t, Easing::Type::EaseInElastic);
+		transform_.scale.y = Easing::Lerp(scaleTween_.startScale.y, scaleTween_.targetScale.y, t, Easing::Type::EaseInElastic);
+		transform_.scale.z = Easing::Lerp(scaleTween_.startScale.z, scaleTween_.targetScale.z, t, Easing::Type::EaseInElastic);
+
+		if (t >= 1.0f) {
+			scaleTween_.active = false;
+		}
+	}
+
 
 	model_->SetTransform(transform_);
 	model_->Update();
+}
+
+void Thorn::SetThornType(ThornType type) {
+	if (type_ == type)
+		return;
+
+	type_ = type;
+
+	// タイプ変更が起きたらイージングを開始する
+	UpgradeThorn();
 }
 
 void Thorn::Draw(Camera useCamera) { model_->Draw(useCamera); }
@@ -55,18 +79,26 @@ void Thorn::TickCooldown() {
 }
 
 void Thorn::UpgradeThorn() {
+	Vector3 newScale;
+
 	switch (type_) {
 	case ThornType::MIN:
-		transform_.scale = {0.5f, 0.5f, 0.5f};
+		newScale = {0.5f, 0.5f, 0.5f};
 		collisionSphere_.radius = 0.25f;
 		break;
 	case ThornType::MIDDLE:
-		transform_.scale = {0.7f, 0.7f, 0.7f};
+		newScale = {0.7f, 0.7f, 0.7f};
 		collisionSphere_.radius = 0.35f;
 		break;
 	case ThornType::MAX:
-		transform_.scale = {1.0f, 1.0f, 1.0f};
+		newScale = {1.0f, 1.0f, 1.0f};
 		collisionSphere_.radius = 0.5f;
 		break;
 	}
+
+	scaleTween_.startScale = transform_.scale;
+	scaleTween_.targetScale = newScale;
+	scaleTween_.duration = 0.6f;
+	scaleTween_.elapsed = 0.0f;
+	scaleTween_.active = true;
 }
