ccf0aba0542f9c24bab87bb26b1f3d71f74d2fbf
AsakawaSakuto <k024g1017@g.neec.ac.jp>
2025-11-27 10:27:44 +0900

    一旦さヴぇ

--- project/Application/SceneManager/IScene.h
+++ project/Application/SceneManager/IScene.h
@@ -13,7 +13,7 @@ enum class SCENE {
 
 class IScene {
 protected:
-    SCENE nowSceneName_ = SCENE::GAME;
+    SCENE nowSceneName_ = SCENE::TITLE;
 
     // AppContext
     AppContext* ctx_ = nullptr;
